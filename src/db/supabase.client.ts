0d50ab40191f69e038d6f2dcce845d66b923952d
Lukasz Sajkowski <lukasz.sajkowski@gmail.com>
2025-11-16 16:06:39 +0100

    feat(db): create Supabase client and export DEFAULT_USER_ID

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -1,8 +1,15 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js"
 
-import type { Database } from '../db/database.types.ts';
+import type { Database } from "./database.types"
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
+const supabaseUrl = import.meta.env.SUPABASE_URL
+const supabaseAnonKey = import.meta.env.SUPABASE_KEY
 
-export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabaseClient = createClient<Database>(
+  supabaseUrl,
+  supabaseAnonKey,
+)
+
+export type SupabaseClient = typeof supabaseClient
+
+export const DEFAULT_USER_ID = "c0802f57-92e4-4673-8904-de11d89c0d7f"
